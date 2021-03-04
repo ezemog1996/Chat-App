14a6c1d5a286674f6c45738b4dc50a633297a866
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-03-04 16:28:19 -0500

    gonna deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const io = require('socket.io')(5000, {
+const io = require('socket.io')(process.env.PORT || 5000, {
     cors: {
-        origin: 3000,
+        origin: 'https://amazingchatapp.herokuapp.com/',
         methods: ['GET', 'POST']
     }
 })
